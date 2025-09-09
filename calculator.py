b635f46defbb4ba8b8a26717138341e2e9f9754e
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-09-09 10:22:18 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,3 +4,21 @@ print("2 = substract")
 print("3 = divide")
 print("4 =  multiply")
 option = int(input("choose an operation: "))
+
+if(option in [1,2,3,4]):
+    num1 = int(input("enter first number: "))
+    num2 = int(input("enter second number: "))
+    
+    if(option == 1):
+        result = num1+ num2
+    elif(option == 2):
+        result = num1 - num2
+    elif(option == 3):
+        result = num1 // num2
+    elif(option == 4):
+        result = num1 * num2
+    
+else:
+    print("Invalid operation entered")
+    
+print("The results is equal to {}".format(result))
